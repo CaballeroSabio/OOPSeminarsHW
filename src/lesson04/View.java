ca71c5b7a2aa06cfe779c4f5befb7edaaca0c6ef
Andrew Glinsky <glinsky_andrew@hotmail.com>
2023-01-13 19:30:49 +0300

    this file was created

--- src/lesson04/View.java
+++ src/lesson04/View.java
@@ -1,2 +1,28 @@
-package lesson04;public class View {
+package lesson04;
+import java.util.Scanner;
+import Controller.Controller;
+
+public class View {
+    private Controller controller;
+    private Scanner scanner;
+
+    public View() {
+        scanner = new Scanner(System.in);
+    }
+
+
+    public void start() {
+        String action = scan();
+        controller.onClick(action);
+    }
+
+    private String scan() {
+        System.out.println(
+                "Что хотите вывести из генеологического дерева (все дерево, первый элемент, последний элемент)?");
+        return scanner.nextLine();
+    }
+
+    public void setController(Controller controller){
+        this.controller = controller;
+    }
 }
