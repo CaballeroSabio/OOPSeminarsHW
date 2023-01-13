476a06bd4c11cad18bd5d986b78e0cf0b1d0d11c
Andrew Glinsky <glinsky_andrew@hotmail.com>
2023-01-13 19:28:38 +0300

    this file was created and modified of mine

--- src/lesson04/model/HumanComparatorByBirth.java
+++ src/lesson04/model/HumanComparatorByBirth.java
@@ -1,2 +1,11 @@
-package lesson04.model;public class HumanComparatorByBirth {
+package lesson04.model;
+import java.util.Comparator;
+
+public class HumanComparatorByBirth<E extends Human> implements Comparator<E>{
+
+    @Override
+    public int compare(E o1, E o2) {
+        return Integer.compare(o1.getBirth(), o2.getBirth());
+    }
+
 }
