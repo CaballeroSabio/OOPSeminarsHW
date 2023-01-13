aeb949f4f65eb1051cb3ca9ac728eec2830dc730
Andrew Glinsky <glinsky_andrew@hotmail.com>
2023-01-13 19:26:30 +0300

    created the new file

--- src/lesson04/controller/Controller.java
+++ src/lesson04/controller/Controller.java
@@ -1,2 +1,30 @@
-package lesson04.controller;public class controller {
+package lesson04.controller;
+
+import Model.Model;
+import View.View;
+
+public class Controller {
+    private View view;
+    private Model model;
+
+    public Controller(View view, Model model) {
+        this.view = view;
+        this.model = model;
+        view.setController(this);
+    }
+
+
+    public void onClick(String action){
+        if (action.equals("all human")){
+            model.print();
+        }
+        else if (action.equals("first human")){
+            model.printFirst();
+        }
+        else if (action.equals("last human")){
+            model.printLast();
+        }
+    }
 }
+
+
