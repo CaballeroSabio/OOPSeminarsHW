f7f62579682b88d2d6537c7634e354eec5a25229
Andrew Glinsky <glinsky_andrew@hotmail.com>
2023-02-07 21:00:07 +0300

    created the new code

--- src/lesson07/human_tree/model/ReadAndWrite.java
+++ src/lesson07/human_tree/model/ReadAndWrite.java
@@ -1,2 +1,6 @@
-package lesson07.human_tree.model;public class ReadAndWrite {
+package lesson07.human_tree.model;
+
+public interface ReadAndWrite {
+    public void write(Object obj, String path) ;
+    public void read(Object obj, String path) ;
 }
