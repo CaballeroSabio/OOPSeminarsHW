4b8d925e65963d289bca91f668e12268c2e8666f
Andrew Glinsky <glinsky_andrew@hotmail.com>
2023-02-07 21:01:55 +0300

    created the new code

--- src/lesson07/human_tree/view/command/PrintHuman.java
+++ src/lesson07/human_tree/view/command/PrintHuman.java
@@ -1,2 +1,29 @@
-package lesson07.human_tree.view.command;public class PrintHuman {
+package lesson07.human_tree.view.command;
+
+import view.View;
+
+public class PrintHuman implements Option{
+    View view;
+
+    public PrintHuman(View view) {
+        this.view = view;
+    }
+
+
+    @Override
+    public String discription() {
+        return "Get human in console";
+    }
+
+    @Override
+    public void execute(int order) {
+        view.printHuman(order);
+    }
+
+
+    @Override
+    public void execute() {
+        System.out.println("Lost id of human");
+        return;
+    }
 }
