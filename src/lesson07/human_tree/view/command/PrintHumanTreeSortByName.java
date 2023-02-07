34e9631db72ae98c202f2f65a70ab0b8ad0d742b
Andrew Glinsky <glinsky_andrew@hotmail.com>
2023-02-07 21:04:55 +0300

    created the new code

--- src/lesson07/human_tree/view/command/PrintHumanTreeSortByName.java
+++ src/lesson07/human_tree/view/command/PrintHumanTreeSortByName.java
@@ -1,2 +1,27 @@
-package lesson07.human_tree.view.command;public class PrintHumanTreeSortByName {
+package lesson07.human_tree.view.command;
+
+import view.View;
+
+public class PrintHumanTreeSortByName  implements Option{
+    view view;
+
+    public PrintHumanTreeSortByName(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public String discription() {
+        return "Get all human in console sort by name.";
+    }
+
+    @Override
+    public void execute() {
+        view.sortHumanByName();
+    }
+
+    @Override
+    public void execute(int order) {
+        System.out.println("Unnecessary id of human");
+        return;
+    }
 }
