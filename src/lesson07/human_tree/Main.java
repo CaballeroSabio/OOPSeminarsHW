6f01342ea2599d7346b5b1dafc6b20753bd5f495
Andrew Glinsky <glinsky_andrew@hotmail.com>
2023-02-07 21:07:57 +0300

    created the new code

--- src/lesson07/human_tree/Main.java
+++ src/lesson07/human_tree/Main.java
@@ -1,2 +1,17 @@
-package lesson07.human_tree;public class Main {
+package lesson07.human_tree;
+
+import java.io.Serializable;
+
+import controller.Controller;
+import model.HumanTreeOperation;
+import view.View;
+
+public class Main implements Serializable{
+    public static void main(String[] args){
+        view view = new View();
+        HumanTreeOperation humanTreeOperation = new HumanTreeOperation();
+        Controller controller = new Controller(view, humanTreeOperation);
+        view.setController(controller);
+        view.start();
+    }
 }
