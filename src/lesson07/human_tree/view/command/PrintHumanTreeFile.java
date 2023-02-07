02b73dc9f9cdcd56c166c0a22b02a49e7d0bcb69
Andrew Glinsky <glinsky_andrew@hotmail.com>
2023-02-07 21:04:08 +0300

    created the new code

--- src/lesson07/human_tree/view/command/PrintHumanTreeFile.java
+++ src/lesson07/human_tree/view/command/PrintHumanTreeFile.java
@@ -1,2 +1,27 @@
-package lesson07.human_tree.view.command;public class PrintHumanTreeFile {
+package lesson07.human_tree.view.command;
+
+import view.View;
+
+public class PrintHumanTreeFile implements Option{
+    View view;
+
+    public PrintHumanTreeFile(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public String discription() {
+        return "Get all human in file.";
+    }
+
+    @Override
+    public void execute() {
+        view.printFile();
+    }
+
+    @Override
+    public void execute(int order) {
+        System.out.println("Unnecessary id of human");
+        return;
+    }
 }
